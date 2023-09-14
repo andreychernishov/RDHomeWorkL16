be3f4ec0a7fd0299163dba72771528fee43a7382
Andrii Chernyshov <andruha46@gmail.com>
2023-09-14 18:26:34 +0300

    working

--- app/src/main/java/com/example/rdhomeworkl16/ApiInterface.kt
+++ app/src/main/java/com/example/rdhomeworkl16/ApiInterface.kt
@@ -5,5 +5,5 @@ import retrofit2.http.GET
 
 interface ApiInterface {
     @GET("/superhero-api/api/all.json")
-    fun getMemes(): Single<HeroResponce>
+    fun getMemes(): Single<List<JsData>>
 }
--- app/src/main/java/com/example/rdhomeworkl16/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/rdhomeworkl16/RecyclerViewAdapter.kt
@@ -7,8 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-
-class RecyclerViewAdapter(private val items:List<JsData>, val callBack:(result: String) -> Unit): RecyclerView.Adapter<RecyclerViewViewHolder>() {
+class RecyclerViewAdapter(var items:List<JsData> = emptyList(), val callBack:(result: String) -> Unit): RecyclerView.Adapter<RecyclerViewViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewViewHolder {
         val listItemViewHolder = LayoutInflater.from(parent.context).inflate(R.layout.rc_item_layout, parent, false)
         return RecyclerViewViewHolder(listItemViewHolder)
@@ -18,17 +17,24 @@ class RecyclerViewAdapter(private val items:List<JsData>, val callBack:(result: 
 
     override fun onBindViewHolder(holder: RecyclerViewViewHolder, position: Int) {
         holder.title.text = items[position].name
+        holder.fullNameTv.text = items[position].biography.fullName
         Glide.with(holder.imageView)
-            .load(items[position].images)
+            .load(items[position].images.sm)
             .into(holder.imageView)
 
         holder.itemView.setOnClickListener {
             callBack(position.toString())
         }
     }
+    fun updateList(modifeedItems: List<JsData>){
+        items = modifeedItems
+        notifyDataSetChanged()
+    }
 
 }
 class RecyclerViewViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
     val title: TextView = itemView.findViewById(R.id.title)
+    val fullNameTv: TextView = itemView.findViewById(R.id.full_name_tv)
     val imageView: ImageView = itemView.findViewById(R.id.image)
+
 }
